0be2d6baf4271a28f16076468bdd927836a4e4ee
Om Deshmukh <omyash1111@gmail.com>
2024-01-26 22:35:28 +0530

    All code implemented

---
+++ temp.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+print(get_available_devices())
