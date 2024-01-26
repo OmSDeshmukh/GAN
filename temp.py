70364b637a6473789839fd14096865d39d37d61e
Om Deshmukh <omyash1111@gmail.com>
2024-01-26 22:41:25 +0530

    Implement running from a saved file

--- temp.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-def get_available_devices():
-    local_device_protos = device_lib.list_local_devices()
-    return [x.name for x in local_device_protos]
-
-print(get_available_devices())
